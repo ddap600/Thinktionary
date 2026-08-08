df2005887881c7c1dd24e0b8c2c75db17158b010
Ddap <diogopinto728@gmail.com>
2026-08-08 14:46:49 +0000

    initial commit

---
+++ thinktionary-backend/thinktionary-backend/src/main/java/com/thinktionary/thinktionary_backend/ThinktionaryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.thinktionary.thinktionary_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinktionaryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThinktionaryBackendApplication.class, args);
+	}
+
+}
---
+++ thinktionary-backend/thinktionary-backend/src/test/java/com/thinktionary/thinktionary_backend/ThinktionaryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinktionary.thinktionary_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinktionaryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
